f4cc6519bbdca306aee8a5e2afb3fbe5df99c568
Tyler McGurrin <mcgurrintyler@gmail.com>
2025-05-07 18:59:40 -0400

    tree hee

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ Tyler McGurrin*/
 
 #define VERSION "RD-00001"
 #define AUTHOR "Xircon"
+#define USAGE "Xircon's Package Manager (XPM):\nVersion               [-v]\nHelp                  [-h]\nInstall               [-i]"
 
 int main(int argC,char *argV[]) {
 	if (argC > 1) {
@@ -14,14 +15,15 @@ int main(int argC,char *argV[]) {
 	            printf("Xircon's Pacage Manager (XPM) %s\nMade By: %s\n", VERSION, AUTHOR);
 	            return true;
 	        } else if (strcmp(argV[i], "-h") == 0) {
-	            printf("Usage: [-v] [-h]\n");
+	            printf("%s\n",USAGE);
 	            return true;
 	        } else {
 	            printf("Unknown option: %s\n", argV[i]);
 	            return false;
 	        }
 	    }
-	}
-
-    return true;
+    } else {
+        printf("%s\n",USAGE);
+	    return true;
+    }
 }
